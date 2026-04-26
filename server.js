c8220d3455d0458e573b20fca39b03fed0535902
parvajvirk <parvajvirk3@gmail.com>
2026-04-25 23:22:36 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const cron = require('node-cron');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('../frontend'));
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../frontend')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
 
 const PORT = process.env.PORT || 3001;
 const UPSTOX_BASE = 'https://api.upstox.com/v2';
